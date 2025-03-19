41f2473fff8b36caa0873ec3e6d0c3d98d355ee3
Ashenafidejene <ashenafidejene75@gmail.com>
2025-03-19 10:24:47 +0300

    add product-2 project in to project and create the class of Product

---
+++ product-2/Product.dart
@@ -0,0 +1,39 @@
+class Product {
+  late String _name;
+  late String _description;
+  late double _price;
+
+  Product(this._name, this._description, this._price);
+
+  String get name => _name;
+
+  set name(String newName) {
+    if (newName.isNotEmpty) {
+      _name = newName;
+    } else {
+      print("The name cannot be null or empty");
+    }
+  }
+
+  String get description => _description;
+
+  set description(String newDescription) {
+    if (newDescription.isNotEmpty) {
+      _description = newDescription;
+    } else {
+      print("The description cannot be null or empty");
+    }
+  }
+
+  double get price => _price;
+
+  set price(double newPrice) {
+    if (newPrice >= 0) {
+      _price = newPrice;
+    } else {
+      print("The price cannot be negative");
+    }
+  }
+}
+
+class ProductManager {}
